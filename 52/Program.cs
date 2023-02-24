6ce5a8d05d99e561a824317b71ca88fd4e7f292a
EkaterinaFedotova <katyaF85@yandex.ru>
2023-02-24 12:32:55 +0300

    Fask 52

--- 52/Program.cs
+++ 52/Program.cs
@@ -1,5 +1,6 @@
 ﻿//Задайте двумерный массив из целых чисел. 
 //Найдите среднее арифметическое элементов в каждом столбце.
+
 using System;
 using static System.Console;
 
@@ -9,113 +10,54 @@ Write("Введите количество строк массива: ");
 int rows = int.Parse(ReadLine());
 Write("Введите количество столбцов массива: ");
 int columns = int.Parse(ReadLine());
-int[,] array = GetArray(rows, columns, 0, 10);
-PrintArray(array);
-double[] averageColumns=GetResultArray(array);
-WriteLine($"Среднее арифметическое каждого столбца = {String.Join("; ",averageColumns)}");
 
-int[,] GetArray(int m, int n, int min, int max)
+int[,] numbers = new int[rows, columns];
+FillArray(numbers);
+PrintArray(numbers);
+
+double[] Numbers = new double[numbers.GetLength(1)];
+
+for (int i = 0; i < numbers.GetLength(1); i++)
 {
-    int[,] result = new int[m, n];
-    for (int i = 0; i < m; i++)
+    double result = 0.0;
+    for (int j = 0; j < numbers.GetLength(0); j++)
     {
-        for (int j = 0; j < n; j++)
-        {
-            result[i, j] = new Random().Next(min, max + 1);
-        }
-
+        result += numbers[j, i];
     }
-    return result;
+    Numbers[i] = result / numbers.GetLength(0);
+}
+PrintArray2(Numbers);
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
 }
 
-void PrintArray(int[,] inArray)
+void PrintArray(int[,] array)
 {
-    for (int i = 0; i < inArray.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < inArray.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            Write($"{inArray[i, j]} ");
+            Write($"{array[i, j]} ");
         }
         WriteLine();
     }
+    WriteLine();
 }
 
-double[] GetResultArray(int[,] array)
+void PrintArray2(double[] array)
 {
-    double[] result = new double[array.GetLength(1)];
-    for (int i = 0; i < array.GetLength(1); i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        double sum=0;
-        for (int j = 0; j < array.GetLength(0); j++)
-        {
-            sum+=array[j,i];
-        }
-        result[i]=Math.Round(sum/array.GetLength(0),2);
+        Write($"{array[i, j]} ");
     }
-    return result;
+    WriteLine();
 }
 
-
-
-// int rows = 3;
-// int colums = 4;
-// int[,] numbers = new int[rows, colums];
-// FillArray2D(numbers);
-// PrintArray2D(numbers);
-
-// double[] avgNumbers = new double[numbers.GetLength(1)];
-
-// for (int i = 0; i < numbers.GetLength(1); i++)
-// {
-//     double result = 0.0;
-//     for (int j = 0; j < numbers.GetLength(0); j++)
-//     {
-//         result += numbers[j, i];
-//     }
-//     avgNumbers[i] = result / numbers.GetLength(0);
-// }
-// PrintArray(avgNumbers);
-
-
-// // Заполнение массива рандомными вещественными числами
-// void FillArray2D(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             array[i, j] = new Random().Next(1, 10);
-//         }
-//     }
-// }
-
-// //  Функция вывода двумерного массива в терминал
-// void PrintArray2D(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             Console.Write(array[i, j] + " ");
-//         }
-//         Console.WriteLine();
-//     }
-//     Console.WriteLine();
-// }
-
-// // Функция вывода массива в терминал 
-// void PrintArray(double[] array)
-// {
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         Console.Write(array[i] + " ");
-//     }
-//     Console.WriteLine();
-// }
-
-// // Функция ввода 
-// int ReadInt(string message)
-// {
-//     Console.Write(message);
-//     return Convert.ToInt32(Console.ReadLine());
-// }
